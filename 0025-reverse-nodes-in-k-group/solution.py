8395205319038329fb6980fa1e2802540dbf13c7
Shikhar Agarwal <shikharagarwal238@gmail.com>
2026-07-09 08:51:22 +0000

    Sync LeetCode submission Runtime - 7 ms (5.29%), Memory - 20.4 MB (46.59%)

---
+++ 0025-reverse-nodes-in-k-group/solution.py
@@ -0,0 +1,44 @@
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+
+        def findNode(node, k):
+            for _ in range(k - 1):
+                node = node.next
+                if not node:
+                    return None
+            return node
+
+        def reverse(node):
+            prev = None
+            curr = node
+            while curr:
+                nxt = curr.next
+                curr.next = prev
+                prev = curr
+                curr = nxt
+
+        temp = head
+        prevNode = None          # fix 2
+
+        while temp:
+            kthnode = findNode(temp, k)
+
+            if kthnode == None:
+                if prevNode:
+                    prevNode.next = temp   # fix 3 — reattach leftover
+                break
+
+            nextNode = kthnode.next
+            kthnode.next = None
+
+            reverse(temp)
+
+            if temp == head:
+                head = kthnode
+            else:
+                prevNode.next = kthnode
+
+            prevNode = temp
+            temp = nextNode
+
+        return head              # fix 4
